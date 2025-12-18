4fe4b14527f2d77024fecdd5d34bb44012c4c760
shohan12727 <aishohan001@gmail.com>
2025-12-18 11:55:22 +0600

    url corrected

--- src/Pages/Dashboard/Payment/SuccessUrl.jsx
+++ src/Pages/Dashboard/Payment/SuccessUrl.jsx
@@ -17,8 +17,6 @@ const SuccessUrl = () => {
   }, [sessionId, axiosSecure]);
 
 
-
-
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
       <div className="bg-white p-10 rounded-2xl shadow-xl text-center max-w-md w-full">
